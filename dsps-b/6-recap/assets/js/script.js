cbeb55c3acf48284c6507943c45a2c42f249ee11
Collin Van der Vorst <collin@littlemissrobot.com>
2022-11-10 10:59:08 +0100

    Recap

---
+++ dsps-b/6-recap/assets/js/script.js
@@ -0,0 +1,29 @@
+var person = {
+    firstname : "Collin",
+    familyname : "Van der Vorst",
+    age : 31,
+    height : 1.94,
+    isUgly: true,
+    partner: {
+        firstname: "Lisa",
+        familyname: "Van der Vorst"
+    },
+    goats: [
+        "T-rex",
+        "Connie",
+        "Clyde",
+        "Collin"
+    ]
+};
+
+var goats = [
+    "T-rex",
+    "Connie",
+    "Clyde",
+    "Collin",
+    "Lisa"
+];
+
+for (var index = 0; index < goats.length; index++) {
+    console.log(goats[index]);
+}
