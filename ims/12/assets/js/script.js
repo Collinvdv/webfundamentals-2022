2378b29b16253c93211656bf0f16fdbbea6bad2a
Collin Van der Vorst <collin@littlemissrobot.com>
2022-12-20 12:59:34 +0100

    Recap

---
+++ ims/12/assets/js/script.js
@@ -0,0 +1,30 @@
+// add 10 circle
+for (var index = 0; index < 10; index++) {
+    addFlag(index);
+}
+
+// when click on flag remove the flag 
+var flags = document.querySelectorAll(".flag");
+
+for (var index = 0; index < 10; index++) {
+    flags[index].addEventListener("click", function () {
+        console.log(this);
+        document.querySelector(".js-flags").removeChild(this);
+    });
+}
+
+
+
+
+function addFlag(number) {
+    var flagEl = document.createElement("div");
+    flagEl.classList.add("flag");
+
+    var circleEl = document.createElement("div");
+    circleEl.classList.add("circle");
+    circleEl.innerHTML = number;
+
+    flagEl.appendChild(circleEl);
+
+    document.querySelector(".js-flags").appendChild(flagEl);
+}
